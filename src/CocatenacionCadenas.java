7b8b47879e536816a565ef1a3880d3cc448735a8
Jose Galvez <jlgalvez77@gmail.com>
2025-01-22 14:08:08 +0100

    first commit

--- src/CocatenacionCadenas.java
+++
@@ -1,7 +0,0 @@
-public class CocatenacionCadenas {
-    public static void main(String[] args) {
-        String nombre = "José";
-        String apellido = "Gálvez";
-        System.out.println(nombre + " " + apellido);
-    }
-}
--- src/Constates.java
+++
@@ -1,9 +0,0 @@
-public class Constates {
-    public static void main(String[] args) {
-        final double PI = 3.1415;
-        final int DIAS_SEMANA = 7;
-        System.out.println("PI = " + PI);
-        System.out.println("DIAS_SEMANA = " + DIAS_SEMANA);
-        System.out.println("Math.PI" + Math.PI);
-    }
-}
